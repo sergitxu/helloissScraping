81a77b062c3e516ace7c4c59faf254c34cec79b2
Sergitxu <sergiortiz@gmail.com>
2019-02-21 20:52:05 +0100

    Crons applied

--- index.js
+++ index.js
@@ -1,129 +1,171 @@
 
+const TOKEN_TELEGRAM = '781813938:AAHPG6npR0OCZUgWrbc6nV98cqFZqPZqnbo';
+const TOKEN_AUDIOSCROBBLER = '0b60a68567872af2073bd9efe40081de';
+const USER_GEONAMES = 'sergitxu';
+const TOKEN_GMAPS = "AIzaSyApZj382B_afAx4ecNtytJFhvWhTf9WvWw";
+
+// Firebase connexion
 var firebase = require('firebase').initializeApp({
- serviceAccount: "./helloiss-service-account.json",
- databaseURL: "https://hello-iss.firebaseio.com/",
+  serviceAccount: "./helloiss-service-account.json",
+  databaseURL: "https://hello-iss.firebaseio.com/",
 });
-
-const cheerio = require ('cheerio');
-const axios = require('axios');
-const TelegramBot = require('node-telegram-bot-api');
-const Telegramtoken = '781813938:AAHPG6npR0OCZUgWrbc6nV98cqFZqPZqnbo';
-
 const database = firebase.database();
+
+// Scraping libraries
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+// Cron jobs library
 const scheduled = require("scheduled");
 
+// Telegram bot setup
+const TelegramBot = require('node-telegram-bot-api');
+const Telegramtoken = TOKEN_TELEGRAM;
+
 // Telegram bot
-const bot = new TelegramBot(Telegramtoken, {polling: true});
-let botMessage='Let me check, ask me in a minute.';
-let botLatitude=45.9645851;
-let botLongitude=63.3030541;
+const bot = new TelegramBot(Telegramtoken, { polling: true });
+let botMessage = 'Let me check, ask me in a minute.';
+let botLatitude = 45.9645851;
+let botLongitude = 63.3030541;
 bot.on('message', (msg) => {
-	bot.sendMessage(msg.chat.id, botMessage);
-	bot.sendLocation(msg.chat.id, latitude=botLatitude, longitude=botLongitude)
+  bot.sendMessage(msg.chat.id, botMessage);
+  bot.sendLocation(msg.chat.id, latitude = botLatitude, longitude = botLongitude)
 });
 
-// Get Crew Image
-const getCrewImage = new scheduled({
-  id: "getCrewImage",
-  pattern: "0 0 * * * * ", // Execute once a day at midnight http://www.nncron.ru/help/EN/working/cron-format.htm
-  task: function getCrewImg() {
-      axios.get('http://www.ariss.org/current-iss-crew.html')
-      .then(response => {
-          const $ = cheerio.load(response.data);
-          const issCrewImg = 'http://www.ariss.org' + $('.galleryImageBorder').attr('src');
-  
-          firebase.database().ref('ISSCrewImage/').set({
-              url: issCrewImg
-          });
-      })
-      .catch(error => {
-          console.log('error', error);
-      });
+const dailyJob = new scheduled({
+  id: "dailyJob",
+  pattern: "0 0 * * * *", // Execute once a day at midnight
+  task: function initDaily() {
+    getCrew();
+    getCrewImg();
+    getISSNews();
   }
 }).start();
 
+const minuteJob = new scheduled({
+  id: "minuteJob",
+  pattern: "*", // Execute once a minute
+  task: function initEachMinute() {
+    locateISS();
+  }
+}).start();
+
+// Get crew info from NASA
+function getCrew() {
+  let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  let xmlHttp = new XMLHttpRequest();
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      let crew = JSON.parse(xmlHttp.responseText).people;
+      database.ref('cosmonaut/').set({
+        crew
+      });
+    } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+      console.error("ERROR! 404");
+      console.info(JSON.parse(xmlHttp.responseText));
+    }
+  };
+  xmlHttp.open("GET", "http://api.open-notify.org/astros.json", true);
+  xmlHttp.send();
+}
+
+// Get Crew Image
+function getCrewImg() {
+  axios.get('http://www.ariss.org/current-iss-crew.html')
+    .then(response => {
+      const $ = cheerio.load(response.data);
+      const issCrewImg = 'http://www.ariss.org' + $('.galleryImageBorder').attr('src');
+
+      database.ref('ISSCrewImage/').set({
+        url: issCrewImg
+      });
+    })
+    .catch(error => {
+      console.log('error', error);
+    });
+}
+
 //Get ISS news
-setInterval(
-  function getISSNews() {
+function getISSNews() {
   axios.get('https://blogs.nasa.gov/spacestation/')
-  .then(response => {
+    .then(response => {
       const $ = cheerio.load(response.data);
 
       var news = {
-          titles: [],
-          urls: [],
-          images: []
+        titles: [],
+        urls: [],
+        images: []
       };
 
-      $('article header h2.entry-title a').each(function() {
+      $('article header h2.entry-title a').each(function () {
         news.titles.push($(this).text());
       });
-      $('article header h2.entry-title a').each(function() {
-        news.urls.push($(this).attr('href'));      
+      $('article header h2.entry-title a').each(function () {
+        news.urls.push($(this).attr('href'));
       });
-      $('article .entry-content figure img').each(function() {
-        news.images.push($(this).attr('src'));       
+      $('article .entry-content figure img').each(function () {
+        news.images.push($(this).attr('src'));
       });
-      firebase.database().ref('ISSNews/').set({
+      database.ref('ISSNews/').set({
         news
       });
-  })
-  .catch(error => {
+    })
+    .catch(error => {
       console.log('error', error);
-  });
-}, 86400000);
+    });
+}
 
 // Get popular music in current country 
 let getCountryMusic = countryName => {
 
   let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
   let xmlHttp = new XMLHttpRequest()
-  let url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${countryName}&api_key=0b60a68567872af2073bd9efe40081de&format=json`
+  let url = `http://ws.audioscrobbler.com/2.0/?method=geo.gettoptracks&country=${countryName}&api_key=${TOKEN_AUDIOSCROBBLER}&format=json`
 
-  xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-        let mostListenedSong = JSON.parse(xmlHttp.responseText);
-        
-        let mostListenedSongName;
-        let mostListenedSongArtist;
-        let mostListenedSongUrl;
-        let mostListenedSongImage;
+  xmlHttp.onreadystatechange = function () {
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      let mostListenedSong = JSON.parse(xmlHttp.responseText);
 
-		if (mostListenedSong.error){
-			// i.e.: Cocos [Keeling] Islands
-			console.error(`error: ${mostListenedSong.error}, ${mostListenedSong.message}`);
-		} else if(mostListenedSong.tracks.track[0] !== undefined){
-			// If there is song info
-			
-				mostListenedSongName = mostListenedSong.tracks.track[0].name;
-				mostListenedSongArtist = mostListenedSong.tracks.track[0].artist.name;
-				mostListenedSongUrl = mostListenedSong.tracks.track[0].url;
-				mostListenedSongImage = mostListenedSong.tracks.track[0].image[2]["#text"];
+      let mostListenedSongName;
+      let mostListenedSongArtist;
+      let mostListenedSongUrl;
+      let mostListenedSongImage;
 
-				firebase.database().ref('song/').set({
-				  name: mostListenedSongName,
-				  artist: mostListenedSongArtist,
-				  url: mostListenedSongUrl,
-				  image: mostListenedSongImage
-				});
+      if (mostListenedSong.error) {
+        // i.e.: Cocos [Keeling] Islands
+        console.error(`error: ${mostListenedSong.error}, ${mostListenedSong.message}`);
+      } else if (mostListenedSong.tracks.track[0] !== undefined && mostListenedSong.tracks.track[0].name && mostListenedSong.tracks.track[0].artist.name && mostListenedSong.tracks.track[0].url && mostListenedSong.tracks.track[0].image[2]["#text"]) {
+        // If there is song info
 
-		}
-		// Empty song values
-		else {
-			//i.e.: British Indian Ocean Territory
-			console.log(`empty song values for ${countryName}`);
-			firebase.database().ref('song/').set({
-			  name: '',
-			  artist: '',
-			  url: '',
-			  image: ''
-			});
-		}
+        mostListenedSongName = mostListenedSong.tracks.track[0].name;
+        mostListenedSongArtist = mostListenedSong.tracks.track[0].artist.name;
+        mostListenedSongUrl = mostListenedSong.tracks.track[0].url;
+        mostListenedSongImage = mostListenedSong.tracks.track[0].image[2]["#text"];
 
-      } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
-          console.error("ERROR! 404");
-          console.info(JSON.parse(xmlHttp.responseText));
+        database.ref('song/').set({
+          name: mostListenedSongName,
+          artist: mostListenedSongArtist,
+          url: mostListenedSongUrl,
+          image: mostListenedSongImage
+        });
+
       }
+      // Empty song values
+      else {
+        //i.e.: British Indian Ocean Territory
+        console.log(`empty song values for ${countryName}`);
+        database.ref('song/').set({
+          name: '',
+          artist: '',
+          url: '',
+          image: ''
+        });
+      }
+
+    } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+      console.error("ERROR! 404");
+      console.info(JSON.parse(xmlHttp.responseText));
+    }
   };
   xmlHttp.open("GET", url, true);
   xmlHttp.send();
@@ -131,130 +173,105 @@ let getCountryMusic = countryName => {
 
 // Get Country Code for ISS location
 let getCountryCode = url => {
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlHttp = new XMLHttpRequest();
-    
-    xmlHttp.onreadystatechange = function() {
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var xmlHttp = new XMLHttpRequest();
 
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-          let ISSCountryLocation = JSON.parse(xmlHttp.responseText);
-          
-			  if (ISSCountryLocation.geonames && ISSCountryLocation.geonames[0] !== undefined) {
-				  
-				console.log(ISSCountryLocation);
-				
-				let ISScountryCode;
-				let ISScountryName;
-				let ISStoponymName;
-				
-				// Check and set country code, country name and toponym name
-				if(ISSCountryLocation.geonames[0].countryCode && ISSCountryLocation.geonames[0].countryName && ISSCountryLocation.geonames[0].adminName1) {
-					
-					ISScountryCode = ISSCountryLocation.geonames[0].countryCode;
-					ISScountryName = ISSCountryLocation.geonames[0].countryName;
-					ISStoponymName = ISSCountryLocation.geonames[0].adminName1;
-					
-					console.log(`countryCode: ${ISScountryCode}, countryName: ${ISScountryName}, toponymName: ${ISStoponymName}`);
-					
-					// message to be sent by Telegram's bot
-					botMessage = `The ISS is flying over ${ISStoponymName} (${ISScountryName}).`;
+  xmlHttp.onreadystatechange = function () {
 
-					firebase.database().ref('currentCountry/').set({
-						code: ISScountryCode,
-						name: ISScountryName,
-						toponym: ISStoponymName
-					});
-					
-					getCountryMusic(ISScountryName);
-					
-				// We miss one of the required country's data
-				} else {
-					console.log('No hay ISScountryCode, ISScountryName o ISStoponymName');
-				}
-				// There is no country location info
-			  } else {
-				  console.log('agua');
-				  // message to be sent by Telegram's bot
-					botMessage = `The ISS is flying somewhere over the sea.`;
-			  }
-		  
-          
-        } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
-            console.error("ERROR! 404");
-            console.info(JSON.parse(xmlHttp.responseText));
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      let ISSCountryLocation = JSON.parse(xmlHttp.responseText);
+
+      if (ISSCountryLocation.geonames && ISSCountryLocation.geonames[0] !== undefined) {
+
+        console.log(ISSCountryLocation);
+
+        let ISScountryCode;
+        let ISScountryName;
+        let ISStoponymName;
+
+        // Check and set country code, country name and toponym name
+        if (ISSCountryLocation.geonames[0].countryCode && ISSCountryLocation.geonames[0].countryName && ISSCountryLocation.geonames[0].adminName1) {
+
+          ISScountryCode = ISSCountryLocation.geonames[0].countryCode;
+          ISScountryName = ISSCountryLocation.geonames[0].countryName;
+          ISStoponymName = ISSCountryLocation.geonames[0].adminName1;
+
+          console.log(`countryCode: ${ISScountryCode}, countryName: ${ISScountryName}, toponymName: ${ISStoponymName}`);
+
+          // message to be sent by Telegram's bot
+          botMessage = `The ISS is flying over ${ISStoponymName} (${ISScountryName}).`;
+
+          database.ref('currentCountry/').set({
+            code: ISScountryCode,
+            name: ISScountryName,
+            toponym: ISStoponymName
+          });
+
+          getCountryMusic(ISScountryName);
+
+          // We miss one of the required country's data
+        } else {
+          console.log('No hay ISScountryCode, ISScountryName o ISStoponymName');
         }
-    };
-    xmlHttp.open("GET", url, true);
-    xmlHttp.send();
-  }
-  
-  // Get ISS current location
-  // execute once a minute
-  setInterval(
-  function locateISS() {
-
-    var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    var xmlHttp = new XMLHttpRequest();
-    
-    xmlHttp.onreadystatechange = function() {
-  
-        if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-          let ISSlocation = JSON.parse(xmlHttp.responseText);
-          if(ISSlocation.message === 'success'){
-            const USERNAME = 'sergitxu';
-            // get ISS latitude and longitude
-            let ISSlatitude = ISSlocation.iss_position.latitude;
-            let ISSlongitude = ISSlocation.iss_position.longitude;
-            
-            botLatitude = ISSlatitude;
-            botLongitude = ISSlongitude;
-  
-            // getCountry based on latitude and longitude
-            let countryCodeUrl = `http://api.geonames.org/findNearbyJSON?username=${USERNAME}&lat=${ISSlatitude}&lng=${ISSlongitude}`;
-  
-            const latlon = `${ISSlatitude},${ISSlongitude}`;
-            // TODO Mostrar recorrido
-            // TODO show a ISS logo in the map
-            const googleMapsKey = "AIzaSyApZj382B_afAx4ecNtytJFhvWhTf9WvWw";
-            const img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=5&size=400x300&sensor=false&key=${googleMapsKey}`;
-
-            firebase.database().ref('currentPosition/').set({
-              urlMap: img_url
-            });
-            getCountryCode(countryCodeUrl);
-          }
-          
-        } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
-            console.error("ERROR! 404");
-            console.info(JSON.parse(xmlHttp.responseText));
-        }
-    };
-    xmlHttp.open("GET", 'http://api.open-notify.org/iss-now.json', true);
-    xmlHttp.send();
-  }, 60000);
-  
-
-  // Get crew info from NASA
-  setInterval(
-  function getCrew() {
-    let XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-      if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-        let crew = JSON.parse(xmlHttp.responseText).people;
-        firebase.database().ref('cosmonaut/').set({
-          crew
-        });
-      } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
-          console.error("ERROR! 404");
-          console.info(JSON.parse(xmlHttp.responseText));
+        // There is no country location info
+      } else {
+        console.log('agua');
+        // message to be sent by Telegram's bot
+        botMessage = `The ISS is flying somewhere over the sea.`;
       }
-    };
-    xmlHttp.open("GET", "http://api.open-notify.org/astros.json", true);
-    xmlHttp.send();
-}, 86400000);
 
-   
+
+    } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+      console.error("ERROR! 404");
+      console.info(JSON.parse(xmlHttp.responseText));
+    }
+  };
+  xmlHttp.open("GET", url, true);
+  xmlHttp.send();
+}
+
+// Get ISS current location
+// execute once a minute
+function locateISS() {
+
+  var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+  var xmlHttp = new XMLHttpRequest();
+
+  xmlHttp.onreadystatechange = function () {
+
+    if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
+      let ISSlocation = JSON.parse(xmlHttp.responseText);
+      if (ISSlocation.message === 'success') {
+        // get ISS latitude and longitude
+        let ISSlatitude = ISSlocation.iss_position.latitude;
+        let ISSlongitude = ISSlocation.iss_position.longitude;
+
+        botLatitude = ISSlatitude;
+        botLongitude = ISSlongitude;
+
+        // getCountry based on latitude and longitude
+        let countryCodeUrl = `http://api.geonames.org/findNearbyJSON?username=${USER_GEONAMES}&lat=${ISSlatitude}&lng=${ISSlongitude}`;
+
+        const latlon = `${ISSlatitude},${ISSlongitude}`;
+        // TODO Mostrar recorrido
+        // TODO show a ISS logo in the map
+        const img_url = `https://maps.googleapis.com/maps/api/staticmap?center=${latlon}&zoom=5&size=400x300&sensor=false&key=${TOKEN_GMAPS}`;
+
+        database.ref('currentPosition/').set({
+          urlMap: img_url
+        });
+        getCountryCode(countryCodeUrl);
+      }
+
+    } else if (xmlHttp.readyState === 4 && xmlHttp.status === 404) {
+      console.error("ERROR! 404");
+      console.info(JSON.parse(xmlHttp.responseText));
+    }
+  };
+  xmlHttp.open("GET", 'http://api.open-notify.org/iss-now.json', true);
+  xmlHttp.send();
+}
+
 //(function(){
 
   // https://blogs.nasa.gov/spacestation/feed/
@@ -262,7 +279,7 @@ let getCountryCode = url => {
 
   // from https://www.nasa.gov/mission_pages/station/expeditions/index.html
  // var crewImage = document.querySelector("div#cards div.bg-card-canvas").style.backgroundImage;
- 
+
  // var missionOverview = document.querySelector("div#ember1127 div div p");
   // var currentMission = meta twitter:title;
  // document.getElementById('currentMissionBadge').src = 'https://www.nasa.gov/sites/default/files/styles/1x1_cardfeed/public/thumbnails/image/iss057-s-001b.jpg';
